44f72498aeb380c742eec419739da9d49dae5ee6
Mendelsom <yuri.babkin@gmail.com>
2024-10-11 12:46:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# 1st program
+print(9**0.5*5)
+# 2nd program
+print(9.99>9.98 and 1000 != 1000.1)
+# 3rd program
+x = 2 * 2 + 2
+y = 2 * (2 + 2)
+print(x == y)
+# 4th program
+x = float ('123.456')
+x = x * 10
+print (int(x % 10))
